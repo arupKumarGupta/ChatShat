a59cbef0413744e891edc58661d2517053f4875c
Arup Kumar Gupta <arup.professional@gmail.com>
2018-05-26 11:23:12 +0530

    added sockets

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '..', '/public');
 const port = process.env.PORT || 3000;
-
 const app = express();
-
 app.use(express.static(publicPath));
-app.listen(port, () => {
+const server = http.createServer(app);
+var io = socketIO(server);
+io.on('connection', (socket) => {
+    console.log('connected');
+    socket.on('disconnect', () => {
+        console.log('disconnected from client');
+    });
+});
+server.listen(port, () => {
     console.log(`Server up on ${port}`);
 });
